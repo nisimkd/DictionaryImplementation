8287bbef46abc4e04f1953e6f7fb5f4c5c6b3321
Nisim Kadosh <nisimkd1@gmail.com>
2024-12-17 23:51:33 +0200

    My Dictionary Implementation

---
+++ DictionaryImplementation/MyDictionary.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography;
+
+namespace DictionaryImplementation
+{
+    public class MyDictionary
+    {
+        #region Private Properties
+
+        private LinkedList<(int, int)>[] _buckets = new LinkedList<(int, int)>[16];
+
+        #endregion
+
+        #region Public Methods
+
+        public void Put(int key, int value)
+        {
+            int bucketIndex = getMD5Hash(key) % _buckets.Length;
+
+            Console.WriteLine($"Key: {key}, Value: {value}, Bucket Index: {bucketIndex}");
+
+            if (_buckets[bucketIndex] == null)
+            {
+                _buckets[bucketIndex] = new LinkedList<(int, int)>();
+                _buckets[bucketIndex].AddFirst((key, value));
+                return;
+            }
+
+            var bucket = _buckets[bucketIndex];
+
+            foreach (var item in bucket)
+            {
+                if (item.Item1 == key)
+                {
+                    bucket.Remove(item);
+                    break;
+                }
+            }
+
+            bucket.AddFirst((key, value));
+        }
+
+        public int Get(int key)
+        {
+            int bucketIndex = getMD5Hash(key) % _buckets.Length;
+
+            if (_buckets[bucketIndex] != null)
+            {
+                var bucket = _buckets[bucketIndex];
+                foreach (var item in bucket)
+                {
+                    if (item.Item1 == key)
+                    {
+                        return item.Item2;
+                    }
+                }
+            }
+
+            throw new KeyNotFoundException($"Key {key} not found");
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private int getMD5Hash(int key)
+        {
+            byte[] inputBytes = BitConverter.GetBytes(key);
+
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                uint unsignedHash = BitConverter.ToUInt32(hashBytes, 0);
+                return (int)(unsignedHash & 0x7FFFFFFF);
+            }
+        }
+
+        #endregion
+    }
+}
--- DictionaryImplementation/Program.cs
+++ DictionaryImplementation/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿var dictionary = new DictionaryImplementation.MyDictionary();
+
+dictionary.Put(17, 209);
+dictionary.Put(82, 580);
+dictionary.Put(1, 90);
+dictionary.Put(18, 607);
+dictionary.Put(16, 37);
+
+dictionary.Put(0, 100);
+dictionary.Put(16, 200);
+dictionary.Put(32, 300);
+
+Console.WriteLine($"Get value for key 17, should be 209:{dictionary.Get(17)}");
+Console.WriteLine($"Get value for key 82, should be 580:{dictionary.Get(82)}");
+Console.WriteLine($"Get value for key 1, should be 90:{dictionary.Get(1)}");
+Console.WriteLine($"Get value for key 18, should be 607:{dictionary.Get(18)}");
+Console.WriteLine($"Get value for key 16, should be 200:{dictionary.Get(16)}");
+Console.WriteLine($"Get value for key 0, should be 100:{dictionary.Get(0)}");
+Console.WriteLine($"Get value for key 16, should be 200:{dictionary.Get(16)}");
+Console.WriteLine($"Get value for key 32, should be 300:{dictionary.Get(32)}");
+
+try
+{
+    dictionary.Get(22);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
